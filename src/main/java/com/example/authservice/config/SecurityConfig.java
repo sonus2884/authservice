e0e97d0844d36f5ea1059057ab3a72bb499d48be
Sonu Kumar <sonus2884@gmail.com>
2020-08-04 07:23:37 +0530

    Authentication

--- src/main/java/com/example/authservice/config/SecurityConfig.java
+++ src/main/java/com/example/authservice/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.example.authservice.config;
 
+import com.example.authservice.service.security.CustomAdminDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,12 +15,21 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    CustomAdminDetailService adminDetailService;
+
     @Bean
     public PasswordEncoder encoder() {
 
         return new BCryptPasswordEncoder(12);
     }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(adminDetailService)
+                .passwordEncoder(encoder());
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -26,7 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .httpBasic()
                 .and()
                 .authorizeRequests()
-                .antMatchers("/allAdmin").hasRole("ADMIN")
+                .antMatchers("/allAdmin").permitAll()
                 .and()
                 .formLogin()
                 .and().rememberMe().disable()
